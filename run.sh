7a736c8909c46fa4a63963a9271dd0f92f65597f
codeWriter9 <nerdsubhankar2006@gmail.com>
2025-04-03 20:18:18 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,13 @@
 helm create hello-world
 ls -ltrga
 helm lint ./hello-world
+kubectl apply -f hello-world/ --dry-run=client
 helm install hello-world ./hello-world --dry-run --debug
 helm upgrade hello-world ./hello-world --dry-run --debug --install
 helm upgrade hello-world ./hello-world --install
+helm get manifest hello-world
 helm list
+helm upgrade hello-world ./hello-world --install --dry-run --debug --set favoriteDrink=coke
 kubectl get pods
 kubectl logs pods/<pod-name>
 kubectl desribe po/<pod-name>
